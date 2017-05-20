94b690c73b0ef96916849e89f51f3f86b8cb4c57
Xgor <real.xgor@hotmail.com>
2017-05-21 01:22:01 +0200

    MainFixed

--- main.lua
+++ main.lua
@@ -1,25 +1,36 @@
 
 local testMap = require "testMap"
 
-local selectedPiece={}
-
+local selectedPiece 
+local tileset
 function love.load()
+	tileset = love.graphics.newImage("gfx/Tileset.png")
+	horseQuad = love.graphics.newQuad(96, 32, 32, 32, tileset:getDimensions())
 	-- Load map
-	loadMap()
+	loadMap(testMap)
 end
 
-function loadMap()
-	currMap = testMap.layers[1].data
-	roomWidth = testMap.width
-	roomHeight = testMap.height
-	for x = 0, 24 do		
-		for y = 0, 14 do
-			if GetMapPiece(x,y) ==10 then
-				selectedPiece.x = x
-				selectedPiece.y = y
-			end
-		end
+function loadMap(map)
+	currMap = map.layers[1].data
+	roomWidth = map.width
+	roomHeight = map.height
+--	for x = 0, 24 do		
+--		for y = 0, 14 do
+--			if GetMapPiece(x,y) ==10 then
+--				SelectPiece(x,y)
+--			end
+--		end
+--	end
+end
+
+function SelectPiece(x,y)
+	if GetMapPiece(x,y) >6 and GetMapPiece(x,y) <13 then
+		selectedPiece = {}
+		selectedPiece.type = GetMapPiece(x,y) 
+		selectedPiece.x =x
+		selectedPiece.y =y
 	end
+	return nil
 end
 
 function GetMapPiece(x,y)
@@ -33,12 +44,18 @@ end
 function love.mousepressed( mouseX, mouseY, button, istouch )
 	local GridPosX = math.floor(mouseX/32)
 	local GridPosY = math.floor(mouseY/32)
-	if CanHorseMoveToPos( GridPosX,GridPosY,
-		selectedPiece.x,selectedPiece.y,currMap) then
+	if selectedPiece == nil then
+		SelectPiece(GridPosX,GridPosY)
+	elseif CanHorseMoveToPos( GridPosX,GridPosY,
+		selectedPiece.x ,selectedPiece.y ,currMap) then
+		print(GridPosY)
+		print(selectedPiece.x)
 		SetMapPiece(selectedPiece.x,selectedPiece.y,0)
 		SetMapPiece(GridPosX,GridPosY,10)
 		selectedPiece.x = GridPosX
 		selectedPiece.y = GridPosY
+	else
+		selectedPiece = nil 
 	end
 end
 
@@ -48,8 +65,6 @@ function love.keypressed(key)
 		love.event.quit()
 	end
 end
-
-
 
 function love.draw()
 	love.graphics.setColor(0,111,111)
@@ -64,23 +79,40 @@ function love.draw()
 					love.graphics.setColor(255,255,255)
 				end
 
-				love.graphics.rectangle("fill", x*32, y*32, 31, 31)
+				love.graphics.rectangle("fill", x*32, y*32, 32, 32)
 				if GetMapPiece(x,y)==3 then
 					love.graphics.setColor(0,100,255)
 					love.graphics.circle("fill", 16+x*32, 16+y*32, 10)
 				elseif GetMapPiece(x,y)==10 then
-					love.graphics.setColor(255,0,0)
-					love.graphics.circle("fill", 16+x*32, 16+y*32, 10)
+					love.graphics.setColor(255,255,255)
+					love.graphics.draw(tileset,horseQuad,x*32,y*32)
+
+--					love.graphics.circle("fill", 16+x*32, 16+y*32, 10)
+				end
+				if selectedPiece ~= nil then
+					if CanHorseMoveToPos(x,y,selectedPiece.x,selectedPiece.y,currMap) then
+						love.graphics.setColor(255,255,0,122)
+						love.graphics.rectangle("fill", x*32, y*32, 32, 32)
+					end
 				end
 			end
 		end
 	end
+	local mouseX,mouseY  = love.mouse.getPosition()
+	love.graphics.setColor(0,0,0)
+	if selectedPiece ~= nil then
+		love.graphics.print(selectedPiece.x,10,10)
+		love.graphics.print(selectedPiece.y,30,10)
+	end
+	love.graphics.print(math.floor(mouseX/32),10,40)
+	love.graphics.print(math.floor(mouseY/32),30,40)
 end
 
 function CanHorseMoveToPos(posX,posY,horseX,horseY,map)
 	local dist
 	if GetMapPiece(posX,posY) ~= nil and GetMapPiece(posX,posY) > 0 then
-		if x ~= horseX and y ~= horseX then
+
+		if x ~= horseX then
 			dist = math.abs( posX-horseX)
 			dist = dist + math.abs( posY-horseY)
 			if(dist == 3) then
